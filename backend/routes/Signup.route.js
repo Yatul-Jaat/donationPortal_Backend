c11a8c7c8a445e60a1bf816886ee5df5442a8dee
Jezorb <yatuljaat0212@gmail.com>
2025-09-21 19:45:17 +0530

    Update Signup.route.js

--- backend/routes/Signup.route.js
+++ backend/routes/Signup.route.js
@@ -1,11 +1,10 @@
 import express from "express"
-import {Signup,NormalLogin,GoogleLogin,Logout} from "../controller/auth.controller.js";
+import {Signup,NormalLogin,GoogleLogin} from "../controller/auth.controller.js";
 
 const router =express.Router();
 
 router.post("/signup",Signup)
 router.post("/login",NormalLogin)
 router.post("/login/google",GoogleLogin)
-router.post("/logout",Logout)
 
 export {router}
